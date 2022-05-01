d826fa6e297682234040ea28cc20136133048d9f
Ivanteev_Kirill <ivanteev87@mail.ru>
2022-05-02 00:21:05 +0300

    создали программу все работает

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трехзначное число.\nДля выхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
